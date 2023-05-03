e566f2a125b1f02ccb461a3d9e7aa25a71d9d814
Smek-one <bertrandbovet@MacBook-Air-de-Cynthia.local>
2023-05-03 22:17:29 +0200

    adding tooltips & advices

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let result = document.querySelector('.result');
 
 
 
+
+
+
 btn.addEventListener('click', (e) => {
     
     //RECUPERATION & CALCUL IMC
@@ -13,23 +16,34 @@ btn.addEventListener('click', (e) => {
     let resultat = parseInt(poids / tailleCalculee);
     result.innerHTML = "Votre IMC est de : " + resultat;
     e.preventDefault(resultat);
+    
 
     //AFFICHAGE CATEGORIE
-    let hint = document.querySelector('.hint');    
+    let hint = document.querySelector('.hint');
+    let conseil = document.querySelector('.conseil');
     let maigre = "Vous êtes maigre";
     let normal = "Vous avez le poids idéal";
     let surpoids = "Vous êtes en surpoids";
     let obese = "Vous êtes en obésité modéré";
     let obeseSevere = "Vous êtes en obésité sévère";
-    
-    if (resultat >16.5 && resultat < 18.5) {
+
+    if(taille === "" || poids === ""){
+        conseil.innerHTML = "Veuillez saisir des données...";
+        result.innerHTML = "";
+    }else if (resultat < 16.5) {
+        hint.innerHTML = "Vérifiez vos données..."        
+    }else if (resultat >16.5 && resultat < 18.5) {
         hint.innerHTML = maigre;
+        conseil.innerHTML = "Votre poids est insuffisant et peut occasionner certains risques pour la santé.";
     }else if (resultat > 18.5 && resultat <25) {
         hint.innerHTML = normal;
+        conseil.innerHTML = "Votre poids n'augmente pas les risques pour votre santé.";
     }else if(resultat > 25 && resultat < 30){
         hint.innerHTML = surpoids;
+        conseil.innerHTML = "Excès de poids pouvant occasionner certains risques pour votre santé.";
     }else if(resultat > 30 && resultat <35){
         hint.innerHTML = obese;
+        conseil.innerHTML = "Obésité, risque accru de développer certaines maladies.";
     }else if(resultat > 35){
         hint.innerHTML = obeseSevere;
     }
@@ -38,3 +52,9 @@ btn.addEventListener('click', (e) => {
     document.form.reset();
     
 });
+
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
