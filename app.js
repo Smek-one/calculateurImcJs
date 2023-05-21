7321b23e34a541ea8e8cc8eaf5b905881635e329
Smek-one <bertrandbovet@gmail.com>
2023-05-21 10:54:30 +0200

    adding images

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ btn.addEventListener('click', (e) => {
     let obese = "Vous êtes en obésité modéré";
     let obeseSevere = "Vous êtes en obésité sévère";
 
+    //AFFICHAGE IMAGE
+    let img = document.querySelector('.img');
+
     if(taille === "" || poids === ""){
         conseil.innerHTML = "Veuillez saisir des données...";
         result.innerHTML = "";
@@ -35,21 +38,38 @@ btn.addEventListener('click', (e) => {
     }else if (resultat >16.5 && resultat < 18.5) {
         hint.innerHTML = maigre;
         conseil.innerHTML = "Votre poids est insuffisant et peut occasionner certains risques pour la santé.";
+        let img1 = document.createElement("img");
+        img1.src = "images/underweight.png";
+        img.appendChild(img1);
     }else if (resultat > 18.5 && resultat <25) {
         hint.innerHTML = normal;
         conseil.innerHTML = "Votre poids n'augmente pas les risques pour votre santé.";
+        let img1 = document.createElement("img");
+        img1.src = "images/normal.png";
+        img.appendChild(img1);
     }else if(resultat > 25 && resultat < 30){
         hint.innerHTML = surpoids;
         conseil.innerHTML = "Excès de poids pouvant occasionner certains risques pour votre santé.";
+        let img1 = document.createElement("img");
+        img1.src = "images/overweight.png";
+        img.appendChild(img1);
     }else if(resultat > 30 && resultat <35){
         hint.innerHTML = obese;
         conseil.innerHTML = "Obésité, risque accru de développer certaines maladies.";
+        let img1 = document.createElement("img");
+        img1.src = "images/obese.png";
+        img.appendChild(img1);
     }else if(resultat > 35){
         hint.innerHTML = obeseSevere;
+        let img1 = document.createElement("img");
+        img1.src = "images/extreme_obese.png";
+        img.appendChild(img1);
     }
     e.preventDefault(hint);
+    
 
-    document.form.reset();
+    document.reset();
+    
     
 });
 
